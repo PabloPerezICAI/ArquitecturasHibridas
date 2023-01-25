dcf1465933fca106f2b949db961f12ea6fbffa28
PabloPerezICAI <202207589@alu.comillas.edu>
2023-01-26 00:04:30 +0100

    Archivos de config y carga de datos

---
+++ cargadatos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+while true
+do
+curl -XPOST localhost:9216/_reindex?pretty -H 'Content-Type: application/json' -d'
+{
+"source": {
+"remote": {
+"host": "http://worker01:9200",
+"username": "consultas",
+"password": "icai4ever"
+},
+"index": "index-data"
+},
+"dest": {
+"index": "index-data"
+}
+}
+'
+sleep 300
+done
