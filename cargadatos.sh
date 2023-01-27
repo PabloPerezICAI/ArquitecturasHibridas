643b1788d47ba68652c76f8a76a69f488499aea3
PabloPerezICAI <112650282+PabloPerezICAI@users.noreply.github.com>
2023-01-27 15:18:31 +0100

    Update cargadatos.sh

--- cargadatos.sh
+++ cargadatos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-curl --user elastic:jopa2023 -XPOST localhost:9216/_reindex?pretty -H 'Content-Type: application/json' -d'
+curl --user elastic:jopa2023 -XPOST -k https://localhost:9216/_reindex?pretty -H 'Content-Type: application/json' -d'
 {
 "source": {
 "remote": {
