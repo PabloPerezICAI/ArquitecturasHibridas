b89acdbb6d63e10ac2710eb8090640c43e6dd531
PabloPerezICAI <202207589@alu.comillas.edu>
2023-03-01 18:07:03 +0100

    export

---
+++ Export.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fechaHoy=$(date '+%Y-%m-%d')
+fechaAyer=$(date '+%Y-%m-%d' -d'1 day ago')
+nombreFichero='/home/alumnos/ah16/export/export-'$(date '+%Y%m%d')'.txt'
+keystore=/home/alumnos/ah16/elasticsearch-7.16.1/elasticsearch/mykeystore.jks
+
+java -jar /home/shared/export.jar -c elastic:icai4ever -f '@timestamp' -from $f$
+
+
+gzip $nombreFichero
