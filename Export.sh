6f1d69cb722160387751dc740e7aaaadf43c9377
PabloPerezICAI <112650282+PabloPerezICAI@users.noreply.github.com>
2023-06-20 00:11:55 +0200

    Update Export.sh

--- Export.sh
+++ Export.sh
@@ -5,6 +5,6 @@ fechaAyer=$(date '+%Y-%m-%d' -d'1 day ago')
 nombreFichero='/home/alumnos/ah16/export/export-'$(date '+%Y%m%d')'.txt'
 keystore=/home/alumnos/ah16/elasticsearch-7.16.1/config/mykeystore.jks
 
-java -jar /home/shared/export.jar -c elastic:icai4ever -f '@timestamp' -from $fechaAyer -i 'index-data' -k $keystore -o $nombreFichero -s icai4ever -to $fechaHoy -u https://localhost:9216
+java -jar /home/shared/export.jar -c elastic:**** -f '@timestamp' -from $fechaAyer -i 'index-data' -k $keystore -o $nombreFichero -s icai4ever -to $fechaHoy -u https://localhost:9216
 
 gzip $nombreFichero
